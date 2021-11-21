de033fc95f7a14fdc0e883b9d098a08018514dbc
zen <zen@zen>
2021-11-20 22:52:16 -0800

    initial

--- crowdaction/main.go
+++ crowdaction/main.go
@@ -2,61 +2,130 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"errors"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
 )
+
+const (
+
+	//date format
+	dateFormat = "20060102"
+
+	//for the time being let's consider 30 days how far in future we retrieve crowdactions
+	//could use a parameter store here
+	//example: current date = 2021.12.01 => retrieve up to and including crowdaction end date=2021.12.30
+	dateLimit = 30
+
+	//access pattern getCrowdaction
+	//item has PK="act#<crowdactionID>" and SK="act#<crowdactionID>"
+	prefixPKcrowdactionID = "act#"
+	prefixSKcrowdactionID = "act#"
+
+	//access pattern getActiveCrowdactions
+	//item has PK="act_end#date_end" and SK="act#<crowdactionID>"
+	prefixPKcrowdaction_date_end = "act_end#"
+
+	//access pattern getEligibleToJoinCrowdactions
+	//item has PK="act_join#date_limit_join" and SK="date_start#act#<crowdactionID>"
+	prefixPKcrowdaction_date_limit_join = "act_join#"
+)
+
+type pkError struct {
+	Pk  string `json:"pk"`
+	Err string `json:"error"`
+}
 
 var (
 	tableName = os.Getenv("CROWDACTION_TABLE")
+	dbClient  *dynamodb.DynamoDB
 )
 
 ////get list of crowd actions
-func getListCrowdaction(req events.APIGatewayV2HTTPRequest) (events.APIGatewayProxyResponse, error) {
-	//TO DO
-	dbc := dynamodbClient()
+func getListCrowdaction(req events.APIGatewayV2HTTPRequest, status string) (events.APIGatewayProxyResponse, error) {
 
-	//https://dynobase.dev/dynamodb-golang-query-examples/#query
+	var pk, sk string
+	var items []*dynamodb.QueryOutput
+	var crowdaction Crowdaction
+	var errors []string
 
-	//scan vs query
+	mapErr := make(map[string]error)
 
-	_, err := dbc.Query(&dynamodb.QueryInput{
-		TableName:              aws.String("CrowdactionTable"),
-		KeyConditionExpression: aws.String("crowdactionID = :hashKey and start_datte > :rangeKey"),
-		ExpressionAttributeValues: map[string]types.AttributeValue{
-			":hashKey":  &types.AttributeValueMemberS{Value: "123"},
-			":rangeKey": &types.AttributeValueMemberN{Value: "20150101"},
-		},
-	})
-	if err != nil {
-		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: http.StatusBadRequest}, nil
+	//get current date
+	dateCurrent := time.Now()
+
+	for i := 0; i < dateLimit; i++ {
+
+		date := dateCurrent.AddDate(0, 0, i).Format(dateFormat)
+		switch status {
+		case "active":
+			pk = prefixPKcrowdaction_date_end + date
+			sk = ""
+		case "joinable":
+			pk = prefixPKcrowdaction_date_limit_join + date
+			sk = dateCurrent.Format(dateFormat)
+		}
+		//get items for a partition key
+		result, err := getItems(pk, sk)
+		//error handling https://play.golang.org/p/D5xTeZP9VnU
+		if err != nil {
+			mapErr[pk] = err
+		}
+		if result != nil {
+			//add partition key result to the total result for the date range
+			items = append(items, result)
+		}
 	}
 
-	out, err := dbc.Scan(&dynamodb.ScanInput{
-		TableName:        aws.String("CrowdactionTable"),
-		FilterExpression: aws.String("attribute_not_exists(deletedAt) AND contains(firstName, :firstName)"),
-		ExpressionAttributeValues: map[string]types.AttributeValue{
-			":firstName": &types.AttributeValueMemberS{Value: "John"},
-		},
-	})
-	if err != nil {
-		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: http.StatusBadRequest}, nil
+	if mapErr != nil {
+		e := pkError{}
+		for k, v := range mapErr {
+			e.Pk = k
+			e.Err = v.Error()
+			jsonErr, _ := json.Marshal(e)
+			errors = append(errors, string(jsonErr))
+		}
+
+		body := "[" + strings.Join(errors, ",") + "]"
+		return events.APIGatewayProxyResponse{Body: body, StatusCode: http.StatusBadRequest}, nil
 	}
 
-	fmt.Println(out.Items)
+	if items == nil {
+		body, _ := json.Marshal(map[string]string{"message": "no active crowdactions found"})
+		return events.APIGatewayProxyResponse{
+			Body:       string(body),
+			StatusCode: http.StatusNotFound,
+		}, nil
+	}
 
-	//no error if we are here
-	var body []byte
+	//no error yet if we are here
+	var listCrowdaction []string
+	for _, v := range items {
+		for _, item := range v.Items {
+			err := dynamodbattribute.UnmarshalMap(item, &crowdaction)
+			if err != nil {
+				return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: http.StatusBadRequest}, nil
+			}
+			c, err := json.Marshal(map[string]interface{}{"data": crowdaction})
+			if err != nil {
+				return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: http.StatusBadRequest}, nil
+			}
+			listCrowdaction = append(listCrowdaction, string(c))
+		}
+	}
+	body := "[" + strings.Join(listCrowdaction, ",") + "]"
 	return events.APIGatewayProxyResponse{
-		Body:       string(body),
+		Body:       body,
 		StatusCode: http.StatusOK,
 	}, nil
 }
@@ -64,16 +133,10 @@ func getListCrowdaction(req events.APIGatewayV2HTTPRequest) (events.APIGatewayPr
 //get details about a crowd action
 func getCrowdaction(crowdactionID string, req events.APIGatewayV2HTTPRequest) (events.APIGatewayProxyResponse, error) {
 
-	dbc := dynamodbClient()
+	var crowdaction Crowdaction
 
-	out, err := dbc.GetItem(&dynamodb.GetItemInput{
-		TableName: &tableName,
-		Key: map[string]*dynamodb.AttributeValue{
-			"crowdactionID": {
-				S: aws.String(crowdactionID),
-			},
-		},
-	})
+	val := prefixPKcrowdactionID + crowdactionID
+	out, err := getItem(val)
 
 	if err != nil {
 		body, err := json.Marshal(map[string]interface{}{"message": err.Error()})
@@ -87,7 +150,7 @@ func getCrowdaction(crowdactionID string, req events.APIGatewayV2HTTPRequest) (e
 	}
 
 	if out.Item == nil {
-		body, err := json.Marshal(map[string]interface{}{"message": "crowdaction does not exist"})
+		body, err := json.Marshal(map[string]string{"message": "crowdaction does not exist"})
 		if err != nil {
 			return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: http.StatusBadRequest}, nil
 		}
@@ -97,8 +160,10 @@ func getCrowdaction(crowdactionID string, req events.APIGatewayV2HTTPRequest) (e
 		}, nil
 	}
 
-	var crowdaction Crowdaction
-	dynamodbattribute.UnmarshalMap(out.Item, &crowdaction)
+	err = dynamodbattribute.UnmarshalMap(out.Item, &crowdaction)
+	if err != nil {
+		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: http.StatusBadRequest}, nil
+	}
 
 	body, err := json.Marshal(map[string]interface{}{"data": crowdaction})
 	if err != nil {
@@ -118,10 +183,25 @@ func handler(req events.APIGatewayV2HTTPRequest) (events.APIGatewayProxyResponse
 		err  error
 	)
 
+	dbClient = dynamodbClient()
+
 	crowdactionID := req.PathParameters["crowdactionID"]
 
 	if crowdactionID == "" {
-		resp, err = getListCrowdaction(req)
+		status := req.QueryStringParameters["status"]
+
+		switch status {
+		case "":
+			status = "joinable" //kind of default
+		case "featured":
+			status = "joinable" //for the time being
+		case "joinable", "active", "ended":
+		default:
+			err := errors.New("unrecognizable status value")
+			return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 400}, nil
+		}
+
+		resp, err = getListCrowdaction(req, status)
 		return resp, err
 	}
 
@@ -132,6 +212,75 @@ func handler(req events.APIGatewayV2HTTPRequest) (events.APIGatewayProxyResponse
 func dynamodbClient() *dynamodb.DynamoDB {
 	sess := session.Must(session.NewSession())
 	return dynamodb.New(sess)
+}
+
+func getItem(val string) (*dynamodb.GetItemOutput, error) {
+
+	result, err := dbClient.GetItem(&dynamodb.GetItemInput{
+		TableName: &tableName,
+		Key: map[string]*dynamodb.AttributeValue{
+			"pk": {
+				S: aws.String(val),
+			},
+			"sk": {
+				S: aws.String(val),
+			},
+		},
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if result.Item == nil {
+		msg := "could not find crowdaction '" + val + "'"
+		return nil, errors.New(msg)
+	}
+
+	return result, nil
+}
+
+func getItems(pk, sk string) (*dynamodb.QueryOutput, error) {
+
+	if sk == "" {
+		keyCond := expression.KeyAnd(
+			expression.Key("pk").Equal(expression.Value(pk))
+		)
+	} else {
+		keyCond := expression.KeyAnd(
+			expression.Key("pk").Equal(expression.Value(pk)),
+			expression.Key("sk").LessThanEqual(expression.Value(sk)),
+		)
+	}
+
+	expr, err := expression.NewBuilder().WithKeyCondition(keyCond).Build()
+	if err != nil {
+		return nil, err
+	}
+
+	/*
+			input := &dynamodb.QueryInput{
+				ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+					":action_end": {S: aws.String(pk)},
+				},
+				KeyConditionExpression: aws.String("pk = :action_end"), aws.String("sk = :action_end"),
+				TableName: &tableName,
+			}
+		}
+	*/
+
+	input := &dynamodb.QueryInput{
+		TableName:                 &tableName,
+		ExpressionAttributeNames:  expr.Names(),
+		ExpressionAttributeValues: expr.Values(),
+		KeyConditionExpression:    expr.KeyCondition(),
+	}
+
+	result, err := dbClient.Query(input)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
 }
 
 func main() {
--- crowdaction/models.go
+++ crowdaction/models.go
@@ -1,10 +1,14 @@
 package main
 
 type Crowdaction struct {
-	CrowdactionID    string `json:"crowdactionID,omitempty"`
-	Title            string `json:"title,omitempty"`
-	DescriptionShort string `json:"description_short,omitempty"`
-	DescriptionLong  string `json:"description_long,omitempty"`
-	StartDate        int    `json:"start_date,omitempty"`
-	EndDate          int    `json:"end_date,omitempty"`
+	CrowdactionID string `json:"crowdactionID,omitempty"`
+	Title         string `json:"title,omitempty"`
+	Description   string `json:"description,omitempty"`
+	Category      string `json:"category,omitempty"`
+	Subcategory   string `json:"subcategory,omitempty"`
+	Location      string `json:"location,omitempty"`
+	DateStart     string `json:"date_start,omitempty"`
+	DateEnd       string `json:"date_end,omitempty"`
+	DateLimitJoin string `json:"date_limit_join,omitempty"`
+	Code          string `json:"code,omitempty"`
 }
