81976b9cce4524029472c94f9b0e1ec0237d9efb
Ruben Horn <horn.ruben@yahoo.de>
2021-12-25 19:50:22 +0100

    comment

--- utils/utf8.go
+++ utils/utf8.go
@@ -6,6 +6,7 @@ import (
 )
 
 func UnescapeUTF8(inStr string) (outStr string, err error) {
+	// Use json.Unmarshal to replace e.g. "\u0026" with "&"
 	jsonStr := `"` + strings.ReplaceAll(inStr, `"`, `\"`) + `"`
 	err = json.Unmarshal([]byte(jsonStr), &outStr)
 	return
