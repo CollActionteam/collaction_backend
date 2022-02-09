869659b6a31bca3cd49b2bcc369e1f5d4b4fb3d6
michel.dias <michel.dias@bemobi.com.br>
2022-02-09 09:10:56 -0300

    using github.com/pkg/errors instead of errors pkg.

--- internal/participation/service.go
+++ internal/participation/service.go
@@ -2,13 +2,12 @@ package participation
 
 import (
 	"context"
-	"errors"
-	"fmt"
 	"github.com/CollActionteam/collaction_backend/internal/constants"
 	m "github.com/CollActionteam/collaction_backend/internal/models"
 	"github.com/CollActionteam/collaction_backend/models"
 	"github.com/CollActionteam/collaction_backend/pkg/repository"
 	"github.com/CollActionteam/collaction_backend/utils"
+	"github.com/pkg/errors"
 )
 
 type Service interface {
@@ -44,7 +43,7 @@ func (s *participationService) GetParticipation(ctx context.Context, userID stri
 
 func (s *participationService) RegisterParticipation(ctx context.Context, userID string, name string, crowdaction *models.Crowdaction, payload m.JoinPayload) error {
 	if !utils.IsFutureDateString(crowdaction.DateLimitJoin) {
-		return fmt.Errorf("cannot change participation for this crowdaction anymore")
+		return errors.New("cannot change participation for this crowdaction anymore")
 	}
 	if err := s.participationRepository.Register(ctx, userID, name, crowdaction, payload); err != nil {
 		return err
@@ -54,7 +53,7 @@ func (s *participationService) RegisterParticipation(ctx context.Context, userID
 
 func (s *participationService) CancelParticipation(ctx context.Context, userID string, crowdaction *models.Crowdaction) error {
 	if !utils.IsFutureDateString(crowdaction.DateEnd) {
-		return fmt.Errorf("cannot change participation for this crowdaction anymore")
+		return errors.New("cannot change participation for this crowdaction anymore")
 	}
 
 	part, err := s.participationRepository.Get(ctx, userID, crowdaction.CrowdactionID)
