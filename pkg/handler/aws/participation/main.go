788817dd3a67176dd027aafe1634ff6e8b4e3f6c
michel.dias <michel.dias@bemobi.com.br>
2022-02-09 21:55:48 -0300

    refactoring participation handler's

--- pkg/handler/aws/participation/main.go
+++ pkg/handler/aws/participation/main.go
@@ -2,14 +2,6 @@ package main
 
 import (
 	"context"
-	"encoding/json"
-	"github.com/CollActionteam/collaction_backend/auth"
-	"github.com/CollActionteam/collaction_backend/internal/constants"
-	m "github.com/CollActionteam/collaction_backend/internal/models"
-	"github.com/CollActionteam/collaction_backend/internal/participation"
-	"github.com/CollActionteam/collaction_backend/models"
-	"github.com/CollActionteam/collaction_backend/pkg/repository"
-	"github.com/CollActionteam/collaction_backend/pkg/repository/aws"
 	"github.com/CollActionteam/collaction_backend/utils"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -19,59 +11,17 @@ import (
 
 func handler(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
 	method := strings.ToLower(req.RequestContext.HTTP.Method)
-	crowdactionID := req.PathParameters["crowdactionID"]
-	var crowdaction *models.Crowdaction
-
-	usrInf, err := auth.ExtractUserInfo(req)
-	if err != nil {
-		return utils.CreateMessageHttpResponse(http.StatusInternalServerError, err.Error()), nil
-	}
-
-	crowdaction, _ = models.GetCrowdaction(crowdactionID, constants.TableName)
-	if crowdaction == nil {
-		return utils.CreateMessageHttpResponse(http.StatusNotFound, "crowdaction not found"), nil
-	}
-
-	participationRepository := repository.NewParticipation(aws.NewDynamo())
-	participationService := participation.NewParticipationService(participationRepository)
-
 	switch method {
 	case "post":
-		var payload m.JoinPayload
-		err = json.Unmarshal([]byte(req.Body), &payload)
-		if err == nil {
-			if len(payload.Commitments) == 0 {
-				return utils.CreateMessageHttpResponse(http.StatusBadRequest, "cannot participate without commitments"), nil
-			}
-			err = participationService.RegisterParticipation(ctx, usrInf.UserID(), usrInf.Name(), crowdaction, payload)
-		}
+		return NewContactHandler().createParticipation(ctx, req)
 	case "delete":
-		err = participationService.CancelParticipation(ctx, usrInf.UserID(), crowdaction)
+		return NewContactHandler().deleteParticipation(ctx, req)
 	case "get":
-		participation, err := participationService.GetParticipation(ctx, usrInf.UserID(), crowdactionID)
-		if err != nil {
-			return utils.CreateMessageHttpResponse(http.StatusInternalServerError, err.Error()), nil
-		}
-		if participation == nil {
-			return utils.CreateMessageHttpResponse(http.StatusNotFound, "not participating"), nil
-		}
-		// "Cannot go wrong"
-		jsonPayload, _ := json.Marshal(participation)
-		return events.APIGatewayV2HTTPResponse{
-			Body:       string(jsonPayload),
-			StatusCode: http.StatusOK,
-		}, nil
-
+		return NewContactHandler().getParticipation(ctx, req)
 	default:
 		return utils.CreateMessageHttpResponse(http.StatusNotImplemented, "not implemented"), nil
 
 	}
-
-	if err != nil {
-		return utils.CreateMessageHttpResponse(http.StatusInternalServerError, err.Error()), nil
-	}
-	return utils.CreateMessageHttpResponse(http.StatusOK, "updated"), nil
-
 }
 
 func main() {
---
+++ pkg/handler/aws/participation/participation.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"github.com/CollActionteam/collaction_backend/auth"
+	"github.com/CollActionteam/collaction_backend/internal/constants"
+	m "github.com/CollActionteam/collaction_backend/internal/models"
+	"github.com/CollActionteam/collaction_backend/internal/participation"
+	"github.com/CollActionteam/collaction_backend/models"
+	"github.com/CollActionteam/collaction_backend/pkg/repository"
+	"github.com/CollActionteam/collaction_backend/pkg/repository/aws"
+	"github.com/CollActionteam/collaction_backend/utils"
+	"github.com/aws/aws-lambda-go/events"
+	"net/http"
+)
+
+type ParticipationHandler struct {
+	service participation.Service
+}
+
+func NewContactHandler() *ParticipationHandler {
+	participationRepository := repository.NewParticipation(aws.NewDynamo())
+	return &ParticipationHandler{service: participation.NewParticipationService(participationRepository)}
+}
+
+func (h *ParticipationHandler) createParticipation(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
+	userID, name, crowdaction, err := retrieveInfoFromRequest(req)
+	if err != nil {
+		return handlerError(err), nil
+	}
+	var payload m.JoinPayload
+	if err := json.Unmarshal([]byte(req.Body), &payload); err != nil {
+		return utils.CreateMessageHttpResponse(http.StatusInternalServerError, err.Error()), nil
+	}
+	if len(payload.Commitments) == 0 {
+		return utils.CreateMessageHttpResponse(http.StatusBadRequest, "cannot participate without commitments"), nil
+	}
+	if err := h.service.RegisterParticipation(ctx, userID, name, crowdaction, payload); err != nil {
+		return utils.CreateMessageHttpResponse(http.StatusInternalServerError, err.Error()), nil
+	}
+	return utils.CreateMessageHttpResponse(http.StatusOK, "updated"), nil
+}
+
+func (h *ParticipationHandler) getParticipation(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
+	userID, _, crowdaction, err := retrieveInfoFromRequest(req)
+	if err != nil {
+		return handlerError(err), nil
+	}
+
+	participation, err := h.service.GetParticipation(ctx, userID, crowdaction.CrowdactionID)
+	if err != nil {
+		return utils.CreateMessageHttpResponse(http.StatusInternalServerError, err.Error()), nil
+	}
+	if participation == nil {
+		return utils.CreateMessageHttpResponse(http.StatusNotFound, "not participating"), nil
+	}
+	// "Cannot go wrong"
+	jsonPayload, _ := json.Marshal(participation)
+	return events.APIGatewayV2HTTPResponse{
+		Body:       string(jsonPayload),
+		StatusCode: http.StatusOK,
+	}, nil
+}
+
+func (h *ParticipationHandler) deleteParticipation(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
+	userID, _, crowdaction, err := retrieveInfoFromRequest(req)
+	if err != nil {
+		return handlerError(err), nil
+	}
+	if err := h.service.CancelParticipation(ctx, userID, crowdaction); err != nil {
+		return utils.CreateMessageHttpResponse(http.StatusInternalServerError, err.Error()), nil
+	}
+	return utils.CreateMessageHttpResponse(http.StatusOK, "updated"), nil
+}
+
+func retrieveInfoFromRequest(req events.APIGatewayV2HTTPRequest) (string, string, *models.Crowdaction, error) {
+	crowdactionID := req.PathParameters["crowdactionID"]
+	usrInf, err := auth.ExtractUserInfo(req)
+	if err != nil {
+		return usrInf.UserID(), usrInf.Name(), nil, err
+	}
+	crowdaction, _ := models.GetCrowdaction(crowdactionID, constants.TableName)
+	if crowdaction == nil {
+		return "", "", nil, errors.New("crowdaction not found")
+	}
+	return usrInf.UserID(), usrInf.Name(), crowdaction, nil
+}
+
+func handlerError(err error) events.APIGatewayV2HTTPResponse {
+	if err.Error() == "crowdaction not found" {
+		return utils.CreateMessageHttpResponse(http.StatusNotFound, err.Error())
+	}
+	return utils.CreateMessageHttpResponse(http.StatusInternalServerError, err.Error())
+}
