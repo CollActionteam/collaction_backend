0423b321cec882d32d33fe45b27bd8d097eae7f7
Ed Reinoso <edgardojesus16@gmail.com>
2022-02-26 09:14:01 +0100

    🚧 [crowdaction] init model and validation on request

--- internal/models/crowdaction.go
+++ internal/models/crowdaction.go
@@ -1,13 +1,32 @@
 package models
 
 type CrowdActionRequest struct {
-	Title       string
-	Description string
-	AppVersion  string
+	Data CrowdActionData `json:"data" validate:"required"`
 }
 
-type CrowdAction struct {
-	CrowdactionID string
-	Title         string
-	Description   string
+type CrowdactionParticipant struct {
+	Name   string `json:"name,omitempty"`
+	UserID string `json:"userID,omitempty"`
+}
+
+type CrowdactionImages struct {
+	Card   string `json:"card,omitempty"`
+	Banner string `json:"banner,omitempty"`
+}
+
+type CrowdActionData struct {
+	CrowdactionID      string                   `json:"crowdactionID"`
+	Title              string                   `json:"title"`
+	Description        string                   `json:"description"`
+	Category           string                   `json:"category"`
+	Subcategory        string                   `json:"subcategory"`
+	Location           string                   `json:"location"`
+	DateStart          string                   `json:"date_start"`
+	DateEnd            string                   `json:"date_end"`
+	DateLimitJoin      string                   `json:"date_limit_join"`
+	PasswordJoin       string                   `json:"password_join"`
+	CommitmentOptions  []CommitmentOption       `json:"commitment_options"`
+	ParticipationCount int                      `json:"participant_count"`
+	TopParticipants    []CrowdactionParticipant `json:"top_participants"`
+	Images             CrowdactionImages        `json:"images"`
 }
--- pkg/handler/aws/crowdaction/main.go
+++ pkg/handler/aws/crowdaction/main.go
@@ -7,10 +7,14 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	// "github.com/CollActionteam/collaction_backend/internal/contact"
+	// "github.com/CollActionteam/collaction_backend/internal/models"
 )
 
 func handler(ctx context.Context, req events.APIGatewayV2HTTPRequest) string {
 	fmt.Println("Hello Go, first message from AWS!")
+
+	// var request models.CrowdActionRequest
 
 	crowdactionID := req.PathParameters["crowdactionID"]
 
