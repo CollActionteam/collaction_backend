39cfa35cb5fc215199904b8e8cb40caa9e7e4ba8
Olayiwola Sobowale <layitheinfotechguru@gmail.com>
2022-03-13 14:17:40 +0100

    Added mock and test

---
+++ pkg/mocks/repository/uploadProfilePictureManager.go
@@ -0,0 +1,23 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/stretchr/testify/mock"
+)
+
+type ProfilePicture struct {
+	mock.Mock
+}
+
+func (p *ProfilePicture) GetUploadUrl(ctx context.Context, ext string, userID string) (string, error) {
+	outputs := p.Mock.Called(ctx, ext, userID)
+
+	uploadUrl := outputs.String(0)
+	err := outputs.Error(1)
+
+	if uploadUrl != "" {
+		return uploadUrl, err
+	}
+	return "", err
+}
