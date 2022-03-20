483e006be11fd0a7580979fa91b88830f98081eb
Ed Reinoso <edgardojesus16@gmail.com>
2022-03-20 17:56:16 +0100

    🚧 [crowdaction] modifying the mock repository to include GetCrowdactionByStatus

--- pkg/mocks/repository/dynamoManager.go
+++ pkg/mocks/repository/dynamoManager.go
@@ -3,7 +3,8 @@ package repository
 import (
 	"context"
 
-	"github.com/CollActionteam/collaction_backend/internal/models"
+	models "github.com/CollActionteam/collaction_backend/internal/models"
+	"github.com/CollActionteam/collaction_backend/utils"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -11,7 +12,12 @@ type Dynamo struct {
 	mock.Mock
 }
 
-func (d *Dynamo) GetCrowdactionById(ctx context.Context, data models.CrowdactionData) error {
-	args := d.Called(ctx, data)
+func (d *Dynamo) GetCrowdactionById(ctx context.Context, crowdactionID string) error {
+	args := d.Called(ctx, crowdactionID)
 	return args.Error(0)
+}
+
+func (d *Dynamo) GetCrowdactionByStatus(ctx context.Context, status string, startFrom *utils.PrimaryKey) ([]models.CrowdactionData, error) {
+	args := d.Called(ctx, status, startFrom)
+	return args.Get(0).([]models.CrowdactionData), args.Error(1)
 }
