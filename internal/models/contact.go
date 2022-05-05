e7df21c0c16cbf5a247f3983f311e2e1963a6f07
Mathias Mogensen <mathias@packaly.com>
2022-05-05 20:58:32 +0200

    fix: add missing dot to regex for contact validation

--- internal/models/contact.go
+++ internal/models/contact.go
@@ -2,9 +2,10 @@ package models
 
 import (
 	"context"
+	"regexp"
+
 	"github.com/CollActionteam/collaction_backend/utils"
 	"github.com/go-playground/validator/v10"
-	"regexp"
 )
 
 type EmailContactRequest struct {
@@ -33,7 +34,7 @@ func (e EmailContactRequest) Validate(ctx context.Context) validator.ValidationE
 		return utils.ValidationResponse(err, validate)
 	}
 
-	reg := regexp.MustCompile(`^(?:ios|android) [0-9]+\.[0-9]+\.[0-9]+\+[0-9]+$`)
+	reg := regexp.MustCompile(`^(?:ios|android) [0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$`)
 	if match := reg.MatchString(e.Data.AppVersion); !match {
 		return validator.ValidationErrorsTranslations{"err": "app version is not valid"}
 	}
